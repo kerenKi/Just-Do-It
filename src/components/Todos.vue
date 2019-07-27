180b285c40c2431c42b1ad6c9791e6fe6b2e4d31
kerenKi <kkinberg13@gmail.com>
2019-07-27 12:09:17 +0200

    TodoItem except todo props

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,15 +2,20 @@
   <div>
     <h1>Todos</h1>
     <div v-bind:key="todo.id" v-for="todo in todos">
-      <h3>{{todo.title}}</h3>
+      <TodoItem  v-bind:todo="todo"/>
     </div>
   </div>  
 </template>
 
 <script>
+  import TodoItem from './TodoItem'
+ 
 export default {
   name:"Todos",
-  props: ["todos"]
+  props: ["todos"],
+  components: {
+    TodoItem
+  }
 }
 </script>
 
