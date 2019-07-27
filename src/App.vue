3429640761ff36dc6c087a8dd345c6747fd1f1f0
kerenKi <kkinberg13@gmail.com>
2019-07-27 11:44:51 +0200

    Display Todos component on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from './components/Todos'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos
+
+  },
+  data() {
+    return {
+      todos:[
+        {
+          id: 1,
+          title: 'Clean the house',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Write some code',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Call Gili',
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    line-height: 1.4;
+  }
+
 </style>
